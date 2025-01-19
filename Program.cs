94d05a7ae979e0bce89480bb3cea669d79f8eb17
Ragahe10 <ragahe10@gmail.com>
2025-01-19 20:19:36 -0300

    Inyeccion de dependencias para la DB

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Inyeccion de dependencia para la cadena de conexion de la base de datos
+var CadenaDeConexion = builder.Configuration.GetConnectionString("SqliteConexion")!.ToString();
+builder.Services.AddSingleton<string>(CadenaDeConexion);
 
 var app = builder.Build();
 
--- obj/Debug/net9.0/backendPFPU.AssemblyInfo.cs
+++ obj/Debug/net9.0/backendPFPU.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendPFPU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5da9e3ae1f5ac368d98f41a3f011a9d625fadb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be6b2dc840acf70ea531f11e050d99825b7ec45")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendPFPU")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendPFPU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
